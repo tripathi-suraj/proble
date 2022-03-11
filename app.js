8b7fee796c00320c297811d55948db14a94bad94
tripathi-suraj <surajtripathi781@gmail.com>
2022-03-11 10:26:07 +0530

    2 files modified, 4 added

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var cluster = require('cluster');
+var express = require('express');
+var ejs = require('ejs');
 process.on('beforeExit', (code) => {
   console.log('Process beforeExit event with code: ', code);
 });
 
 process.on('uncaughtException', function (err, origin) {
-  console.log(process.stderr.fd);
-  fs.writeSync(
-    process.stderr.fd,
-    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  console.error(
+    new Date().toUTCString() + ' uncaughtException:',
+    err.message,
+    err.stack
   );
+  process.exit(1);
 });
 
 process.on('exit', (code) => {
@@ -22,12 +25,22 @@ setTimeout(() => {
 }, 500);
 
 // Intentionally cause an exception, but don't catch it.
-nonexistentFunc();
 console.log('This will not run.');
+var app = express();
+process.app = app;
 
-http
-  .createServer(function (req, res) {
-    res.write('hello');
-    res.end();
-  })
-  .listen(3000);
+app.vars = {};
+app.vars.basepath = __dirname;
+//set view using express
+app.set('PORT', 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+console.log(fs.existsSync(app.vars.basepath + '/env'));
+
+try {
+  require('./routes/index').attachRoute(app);
+} catch (e) {
+  console.log(e);
+}
+
+app.listen(app.get('PORT'));
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var routes = {};
+
+routes.home = function (req, res) {
+  res.render('home', { title: 'Home Api' });
+};
+
+routes.version = function (req, res) {
+  res.send('api ver 20.0');
+};
+
+exports.attachRoute = function (app) {
+  app.get('/', routes.home);
+  app.get('/v', routes.version);
+};

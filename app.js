632fea364c8674110131cf480161bc2582df0d73
tripathi-suraj <surajtripathi781@gmail.com>
2022-03-13 22:11:18 +0530

    simple functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var cluster = require('cluster');
 var express = require('express');
 var ejs = require('ejs');
+
 process.on('beforeExit', (code) => {
   console.log('Process beforeExit event with code: ', code);
 });
